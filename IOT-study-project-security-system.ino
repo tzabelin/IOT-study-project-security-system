0c22ed8a6b322b96e357801174ec859fd1d15c82
pete <pete@nomail>
2022-04-23 23:51:52 +0300

    Project structure update

--- IOT-study-project-security-system.ino
+++ IOT-study-project-security-system.ino
@@ -2,9 +2,19 @@
 #include <Wire.h>
 #include "Arduino.h"
 #include "PCF8574.h"
+#include "ESP8266WiFi.h"
+
+/* ### DEFINE ### */
+
+#define RX 8
+#define TX A0
+#define lcdColumns 16
+#define lcdRows 2
 
 // For arduino uno only pin 1 and 2 are interrupted
 #define ARDUINO_UNO_INTERRUPTED_PIN D3
+
+/* ### GLOBAL OBJECTS ### */
 
 // Function interrupt
 void ICACHE_RAM_ATTR  keyPressedOnPCF8574();
@@ -12,15 +22,42 @@ void ICACHE_RAM_ATTR  keyPressedOnPCF8574();
 // Set i2c address
 PCF8574 pcf8574(0x38, ARDUINO_UNO_INTERRUPTED_PIN, keyPressedOnPCF8574);
 
- 
-#define RX 8
-#define TX A0
-#define lcdColumns 16
-#define lcdRows 2
-
-
 LCD_I2C lcd(0x3f, lcdColumns, lcdRows);//standart addresses are 0x3f or 0x27
 
+/* ### PCF8574 MULTIPLEXERS ### */
+
+unsigned long timeElapsed;
+void initialize_PCF8574()
+{
+  pcf8574.pinMode(P0, INPUT);
+  pcf8574.pinMode(P1, INPUT_PULLUP);
+  pcf8574.pinMode(P2, INPUT);
+  pcf8574.pinMode(P3, INPUT);
+  timeElapsed = millis();
+}
+
+bool keyPressed = false;
+void read()
+{
+  if (keyPressed)
+  {
+    uint8_t val0 = pcf8574.digitalRead(P0);
+    uint8_t val1 = pcf8574.digitalRead(P1);
+    uint8_t val2 = pcf8574.digitalRead(P2);
+    uint8_t val3 = pcf8574.digitalRead(P3);
+    keyPressed= false;
+  }
+}
+
+void keyPressedOnPCF8574(){
+  // Interrupt called (No Serial no read no wire in this function, and DEBUG disabled on PCF library)
+  print_LCD("Interrupt");
+  delay(1000);
+   keyPressed = true;
+
+}
+
+/* ### I2C SCANNER ### */
 
 int scanner() 
 {
@@ -63,6 +100,7 @@ byte find_nth_device(int n)
   return 0;   
 }
 
+/* ### 1604 LCD ### */
 
 inline void print_LCD(const char* str, int n)
 {
@@ -87,6 +125,7 @@ inline int ask_for_input(const char *str)
     //TODO buttons press
 }
 
+/* ### MAIN SUPERLOOP ### */
 
 void setup() 
 {
@@ -102,40 +141,6 @@ void setup()
   initialize_PCF8574();
 }
 
-unsigned long timeElapsed;
-void initialize_PCF8574()
-{
-
-  pcf8574.pinMode(P0, INPUT);
-  pcf8574.pinMode(P1, INPUT_PULLUP);
-  pcf8574.pinMode(P2, INPUT);
-  pcf8574.pinMode(P3, INPUT);
-  timeElapsed = millis();
-}
-
-
-bool keyPressed = false;
-void read()
-{
-  if (keyPressed)
-  {
-    uint8_t val0 = pcf8574.digitalRead(P0);
-    uint8_t val1 = pcf8574.digitalRead(P1);
-    uint8_t val2 = pcf8574.digitalRead(P2);
-    uint8_t val3 = pcf8574.digitalRead(P3);
-    keyPressed= false;
-  }
-}
-
-
-
-void keyPressedOnPCF8574(){
-  // Interrupt called (No Serial no read no wire in this function, and DEBUG disabled on PCF library)
-  print_LCD("Interrupt");
-  delay(1000);
-   keyPressed = true;
-
-}
 void loop() 
 { 
   read();
